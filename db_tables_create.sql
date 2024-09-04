405f5543627fb61d717e3e29b71282939b9c8400
VijayPatil86 <vijipatil090386@gmail.com>
2024-09-04 14:28:39 +0530

    added unique constraint on column CATEGORY_NAME

--- db_tables_create.sql
+++ db_tables_create.sql
@@ -1,5 +1,5 @@
 create table CATEGORY(
 	CATEGORY_ID  smallserial primary key,
-	CATEGORY_NAME varchar(50) not null,
+	CATEGORY_NAME varchar(50) not null unique,
 	CATEGORY_VISIBILE  varchar(1) check (CATEGORY_VISIBILE in ('Y', 'N')) default 'Y'
 );
