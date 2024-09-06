9779283bdea10433cc001a63c627d04d77b95ac4
VijayPatil86 <vijipatil090386@gmail.com>
2024-09-06 07:29:09 +0530

    table data changed

--- db_tables_insert.sql
+++ db_tables_insert.sql
@@ -1,7 +1,2 @@
-insert into CATEGORY(CATEGORY_NAME) values ('Foodgrains');
-insert into CATEGORY(CATEGORY_NAME) values ('Oil');
-insert into CATEGORY(CATEGORY_NAME) values ('Masala');
-insert into CATEGORY(CATEGORY_NAME) values ('Dairy');
-insert into CATEGORY(CATEGORY_NAME) values ('Bakery');
-insert into CATEGORY(CATEGORY_NAME) values ('Fruits');
-insert into CATEGORY(CATEGORY_NAME) values ('Vegetables');
+insert into CATEGORY (CATEGORY_NAME) values ('Dairy');
+insert into CATEGORY (CATEGORY_NAME) values ('Bakery');
