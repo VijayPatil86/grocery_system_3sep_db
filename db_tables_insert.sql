6c4647a0353f94cfba775a36d49b4ac1603a1455
VijayPatil86 <vijipatil090386@gmail.com>
2024-09-06 14:37:35 +0530

    added data for tables - DAIRY_PRODUCT, BAKERY_PRODUCT

--- db_tables_insert.sql
+++ db_tables_insert.sql
@@ -1,2 +1,10 @@
 insert into CATEGORY (CATEGORY_NAME) values ('Dairy');
 insert into CATEGORY (CATEGORY_NAME) values ('Bakery');
+
+insert into DAIRY_PRODUCT(DAIRY_PRODUCT_NAME, CATEGORY_ID) values ('Milk', 1);
+insert into DAIRY_PRODUCT(DAIRY_PRODUCT_NAME, CATEGORY_ID) values ('Buttermilk', 1);
+insert into DAIRY_PRODUCT(DAIRY_PRODUCT_NAME, CATEGORY_ID) values ('Curd', 1);
+
+insert into BAKERY_PRODUCT(BAKERY_PRODUCT_NAME, CATEGORY_ID) values ('Biscuit', 2);
+insert into BAKERY_PRODUCT(BAKERY_PRODUCT_NAME, CATEGORY_ID) values ('Cake', 2);
+insert into BAKERY_PRODUCT(BAKERY_PRODUCT_NAME, CATEGORY_ID) values ('Bread', 2);
