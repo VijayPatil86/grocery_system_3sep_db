a5d11b68adec60ffe06369904c584f11b5e36ebd
VijayPatil86 <vijipatil090386@gmail.com>
2024-09-07 20:59:24 +0530

    added table MILK_BRAND

--- db_tables_create.sql
+++ db_tables_create.sql
@@ -19,3 +19,12 @@ create table BAKERY_PRODUCT (
 	CATEGORY_ID int,
 	constraint "fk_CATEGORY_ID" foreign key (CATEGORY_ID) references CATEGORY(CATEGORY_ID) on delete cascade
 );
+
+create table MILK_BRAND(
+	MILK_BRAND_ID smallserial primary key,
+	MILK_BRAND_NAME varchar(50) not null,
+	PACKAGING varchar(50) not null,
+	MILK_BRAND_AVAILABLE char(1) not null check(MILK_BRAND_AVAILABLE in ('Y', 'N')) default 'Y',
+	DAIRY_PRODUCT_ID int not null,
+	constraint "fk_DAIRY_PRODUCT_ID" FOREIGN KEY (DAIRY_PRODUCT_ID) REFERENCES DAIRY_PRODUCT(DAIRY_PRODUCT_ID) on delete cascade
+);
